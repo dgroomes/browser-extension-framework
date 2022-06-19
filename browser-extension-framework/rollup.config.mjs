2029ed22902ed8883c70298fcb03c8072556a56c
David Groomes <dwgroomes@gmail.com>
2022-06-19 12:41:29 -0500

    Resolve Rollup warnings about 'undefined this'

--- browser-extension-framework/rollup.config.mjs
+++ browser-extension-framework/rollup.config.mjs
@@ -1,4 +1,14 @@
-export default [
+/**
+ * Set the "context" to "window" to avoid the Rollup warning https://rollupjs.org/guide/en/#error-this-is-undefined.
+ * BrowserExtensionFramework runs in browsers, so 'window' is the correct thing to refer to the "global this".
+ *
+ * See a general discussion about Rollup's "context" here https://github.com/rollup/rollup/issues/3788
+ */
+function contextWindow(config) {
+    config.context = "window"
+}
+
+const config = [
     {
         input: 'transpiled/index.js',
         output: {
@@ -14,3 +24,8 @@ export default [
         }
     }
 ]
+
+// Apply common config
+config.map(config => contextWindow(config))
+
+export default config
